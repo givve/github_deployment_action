214f66389379abb10dfff02eaec8ca12b7e736e6
Fabian Locker <fabian.locker@givve.com>
2024-10-22 09:56:33 +0200

    chore: git noob

---
+++ src/github.ts
@@ -0,0 +1,41 @@
+const axios = require('axios')
+
+const octoAuth = require('@octokit/auth-action')
+const Request = require('@octokit/request')
+
+import * as core from '@actions/core'
+
+export class GitHub {
+  private requestWithAuth: any
+
+  constructor() {}
+
+  async performAuth() {
+    const auth = octoAuth.createActionAuth()
+    const authentication = await auth()
+
+    this.requestWithAuth = Request.request.defaults({
+      request: {
+        hook: auth.hook
+      },
+      mediaType: {
+        previews: ['machine-man']
+      }
+    })
+  }
+
+  async getLabels(): Promise<string[]> {
+    return new Promise(async (resolve, reject) => {
+      const { data: issue } = await this.requestWithAuth(
+        'GET /repos/{owner}/{repo}/issues/{pull_number}',
+        {
+          owner: 'givve',
+          repo: 'givve',
+          pull_number: core.getInput('pull_request')
+        }
+      )
+
+      resolve(issue.labels.map((label: any) => label.name))
+    })
+  }
+}
---
+++ src/semaphore.ts
@@ -0,0 +1,36 @@
+const axios = require('axios')
+import * as core from '@actions/core'
+
+// TEMP: Will be implemented via action inputs
+const semaphoreAPI = core.getInput('semaphoreAPI')
+const semaphoreAPIKey = core.getInput('semaphoreAPIKey')
+export async function getLocks(component: string): Promise<any> {
+  return axios.get(
+    semaphoreAPI +
+      `/api/products/5f7427d977b4b64aeabad92d/components/${component}/locks`,
+    {
+      headers: {
+        Authorization: semaphoreAPIKey
+      }
+    }
+  )
+}
+
+export async function setLock(component: string): Promise<any> {
+  return axios.post(
+    semaphoreAPI +
+      `/api/products/5f7427d977b4b64aeabad92d/components/${component}/locks`,
+    {
+      lock: {
+        created_by: 'github_action',
+        purpose: 'manual deployment lock',
+        expires_at: null
+      }
+    },
+    {
+      headers: {
+        Authorization: semaphoreAPIKey
+      }
+    }
+  )
+}
